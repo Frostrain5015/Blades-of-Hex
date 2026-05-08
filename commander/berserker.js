7477a1dbe8d3b0658d446d57a759fdc9cfbc88db
FRAIN55015 <frostrain5015@users.noreply.github.com>
2026-05-08 20:04:44 +0800

    feats. 微调了狂战士的相关数值

--- commander/berserker.js
+++ commander/berserker.js
@@ -3,25 +3,26 @@ export default {
   id: 'berserker',
   name: '狂战士',
   skill: '狂暴',
-  hpBonus: 40, atkBonus: 25, spdBonus: 0,
-  desc: '主动技能【狂暴】：攻击力+10，防御力+15%，持续2回合。（冷却3回合）',
+  hpBonus: 35, atkBonus: 15, spdBonus: 0,
+  desc: '主动技能【狂暴】：攻击力+20，防御力+25%，持续2回合。（冷却3回合）',
 
   activeSkill: {
     name: '狂暴',
-    desc: '攻击力+10，防御力+15%，持续2回合',
+    desc: '攻击力+20，防御力+25%，持续2回合',
     duration: 2,
     cooldown: 3,
-    buffs: { atk: 10, def: 0.15 },
+    buffs: { atk: 20, def: 0.25 },
 
     onActivate(unit, helpers) {
-      unit._atkBonus += 10;
-      unit._activeSkillBuffs = { atk: 10, def: 0.15 };
+      const b = this.buffs;
+      unit._atkBonus += b.atk;
+      unit._activeSkillBuffs = { atk: b.atk, def: b.def };
       helpers.spawnFx(unit.tile.x, unit.tile.y, '💢', '狂暴');
-      helpers.logMessage('狂战士【狂暴】激活：+10ATK +15%防御，持续2回合');
+      helpers.logMessage(`狂战士【狂暴】激活：+${b.atk}ATK +${Math.round(b.def * 100)}%防御，持续${this.duration}回合`);
     },
 
     onExpire(unit, helpers) {
-      unit._atkBonus -= 10;
+      unit._atkBonus -= this.buffs.atk;
       unit._activeSkillBuffs = null;
       helpers.logMessage('狂战士【狂暴】效果结束');
     }
